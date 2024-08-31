d86ffbf5a7e11acd5694da29379c28de0bcc3f01
gathuitapius <gathuitapius097@gmail.com>
2024-08-31 16:51:19 +0300

    initialize CRUD operations for blogs

--- backend/routes/blogs.js
+++ backend/routes/blogs.js
@@ -7,13 +7,28 @@ const router = express.Router();
 
 //Get all Blogs
 router.get('/', (req, res) =>{
-    res.json({mssg : "Welcome to U-Blog!"})
+    res.json({mssg : "GET all blogs"})
 })
 
 
-//Gett
+//Get a single blog
 router.get('/:id', (req, res) =>{
-    res.json({mssg : "Welcome to U-Blog!"})
+    res.json({mssg : "GET single Blog"})
+})
+
+//create new blog
+router.post('/', (req, res) =>{
+    res.json({mssg : "CREATE new Blog"})
+})
+
+//create new blog
+router.put('/:id', (req, res) =>{
+    res.json({mssg : "UPDATE a Blog"})
+})
+
+//delete blog
+router.delete('/:id', (req, res) =>{
+    res.json({mssg : "DELETE a Blog"})
 })
 
 export default router;
