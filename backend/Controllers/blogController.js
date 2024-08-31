2dbed9e987f24bfe483b8304ebdf5e4861fbcf0a
gathuitapius <gathuitapius097@gmail.com>
2024-08-31 18:32:21 +0300

    created Blog model Schema for my blogs

--- backend/Controllers/blogController.js
+++ backend/Controllers/blogController.js
@@ -1,2 +1,25 @@
 import Blog from '../models/blogs.js'
 
+//GET all Blogs
+//GET a single Blog
+//CREATE a Blog
+
+const createBlog = () =>{
+    const {title, blog_image, content} = req.body;
+
+    try{
+        const blog = await Blog.create({title, blog_image, content});
+        res.status(200).json(blog);
+
+    } catch(error){
+        res.status(400).json({error: error.message})
+    }
+    res.json({mssg : "CREATE new Blog"})
+}
+
+//DELETE a Blog
+
+
+exports default = {
+    createBlog,
+}
--- backend/routes/blogs.js
+++ backend/routes/blogs.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import Blog from '../models/blogs.js'
+import {createBlog} from '../controllers/blogController.js'
 
 
 const router = express.Router();
@@ -17,18 +17,7 @@ router.get('/:id', (req, res) =>{
 })
 
 //create new blog
-router.post('/', async (req, res) =>{
-    const {title, blog_image, content} = req.body;
-
-    try{
-        const blog = await Blog.create({title, blog_image, content});
-        res.status(200).json(blog);
-
-    } catch(error){
-        res.status(400).json({error: error.message})
-    }
-    res.json({mssg : "CREATE new Blog"})
-})
+router.post('/', CreateBlog);
 
 //create new blog
 router.put('/:id', (req, res) =>{
